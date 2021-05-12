a7c9cb228e54548715671c9537fcb73400bc6ccd
Guillaume Zgajnar <guillaume.zgajnar@gonexa.fr>
2021-05-12 18:02:54 +0200

    Creation de la map (sans img)

---
+++ front-brpwa/src/components/Cell.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="cell"  v-on:click="clickCell">
+
+  </div>
+</template>
+<script>
+export default {
+  name: "Cell",
+
+  props: {
+    backgroundTile: Number,
+    x: Number,
+    y: Number,
+    obstacleTile: Number,
+  },
+  methods :{
+      clickCell(){
+          if(!this.obstacle){
+             var data = { x : this.x , y : this.y};
+             this.$emit('cell-clicked',data);
+          }
+      }
+  }
+};
+</script>
+
+<style scoped>
+.cell {
+  height: 24px;
+  width: 24px;
+}
+
+.cell:hover{
+ cursor:pointer;
+}
+</style>
---
+++ front-brpwa/src/views/Game.vue
@@ -0,0 +1,18 @@
+<template>
+
+<div>
+  <h1>Game</h1>
+
+  <grid></grid> 
+</div>
+</template>
+<script>
+
+import Grid from '../components/Grid.vue';
+
+export default {
+  components: { Grid },
+  name: "Game",
+ 
+};
+</script>
