8eecdfcfa4da066099f8af8092d39d8e484a72df
{ChooseYourName} <narayanin.alec@hotmail.com>
2021-06-22 11:41:33 +0200

    maj server.js et suppresion postinstall script

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const serveStatic = require('serve-static');
 const path = require('path');
-app = express();
+const port = process.env.PORT || 8080;
+const app = express();
+
 app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT || 80;
+
+app.get(/.*/, function (req, res) {
+  res.sendFile(path.join(__dirname, '/dist/index.html'));
+});
 app.listen(port);
