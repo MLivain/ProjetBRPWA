ba26edbbfb5934a24b27bc144b36dcb16236ac11
{ChooseYourName} <narayanin.alec@hotmail.com>
2021-06-22 11:22:02 +0200

    mise en place serveur express pour deploiement

--- front-brpwa/src/services/config.js
+++ front-brpwa/src/services/config.js
@@ -2,6 +2,6 @@ export default function config() {
   if (process.env.NODE_ENV === 'development') {
     return { apiUrl: 'https://localhost:44383' };
   } else {
-    return { apiUrl: '' };
+    return { apiUrl: 'https://brpwaback.azurewebsites.net' };
   }
 }
